1fb59135e0f99ba87dca7782d414ec1b933f4453
root <root@DESKTOP-ECSQPB1.localdomain>
2019-05-30 19:44:15 +0200

    add full sample

---
+++ outputs.tf
@@ -0,0 +1,83 @@
+output "landing_zone_pipeline_s3_bucket_id" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_pipeline_s3_bucket_id"]}"
+}
+
+output "landing_zone_pipeline_s3_bucket_arn" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_pipeline_s3_bucket_arn"]}"
+}
+
+output "landing_zone_pipeline_s3_bucket_bucket" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_pipeline_s3_bucket_bucket"]}"
+}
+
+output "landing_zone_pipeline_artifact_s3_bucket_id" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_pipeline_artifact_s3_bucket_id"]}"
+}
+
+output "landing_zone_pipeline_artifact_s3_bucket_arn" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_pipeline_artifact_s3_bucket_arn"]}"
+}
+
+output "landing_zone_pipeline_artifact_s3_bucket_bucket" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_pipeline_artifact_s3_bucket_bucket"]}"
+}
+
+output "landing_zone_organization_accounts_id" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_organization_accounts_id"]}"
+}
+
+output "landing_zone_organization_accounts_arn" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_organization_accounts_arn"]}"
+}
+
+output "landing_zone_organization_id" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_organization_id"]}"
+}
+
+output "landing_zone_organization_accounts" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_organization_accounts"]}"
+}
+
+output "landing_zone_organization_roots" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_organization_roots"]}"
+}
+
+output "landing_zone_code_pipeline_role_id" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_code_pipeline_role_id"]}"
+}
+
+output "landing_zone_code_pipeline_role_name" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_code_pipeline_role_name"]}"
+}
+
+output "landing_zone_code_pipeline_id" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_code_pipeline_id"]}"
+}
+
+output "landing_zone_code_pipeline_arn" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_code_pipeline_arn"]}"
+}
+
+output "landing_zone_code_build_role_id" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_code_build_role_id"]}"
+}
+
+output "landing_zone_code_build_role_name" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_code_build_role_name"]}"
+}
+
+output "landing_zone_code_build_role_arn" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_code_build_role_arn"]}"
+}
+
+output "landing_zone_code_build_id" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_code_build_id"]}"
+}
+
+output "landing_zone_code_build_badge_url" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_code_build_badge_url"]}"
+}
+
+output "landing_zone_code_build_arn" {
+  value = "${module.example-create-s3-and-codepipeline.landing_zone["landing_zone_code_build_arn"]}"
+}
