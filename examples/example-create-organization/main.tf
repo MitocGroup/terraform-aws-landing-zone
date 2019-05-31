df73f8bb08e95c6761b959c766e43f1b9a64dffa
Eugene I <eugene@mitocgroup.com>
2019-05-31 16:21:24 -0400

    refactor using terraform-aws-landing-zone s3 bucket

--- examples/example-create-organization/main.tf
+++ examples/example-create-organization/main.tf
@@ -1,7 +1,7 @@
 module "example-create-organization" {
   source = "../../modules/landing_zone"
   landing_zone_components = {
-      landing_zone_organization = "s3://{BUCKET NAME}/{BUCKET KEY}/default.tfvars"
+      landing_zone_organization = "s3://terraform-aws-landing-zone/example/landing_zone_organization/default.tfvars"
   }
   region = "us-east-1"
   account_id = "123456789012"
--- examples/example-create-s3-and-codepipeline/main.tf
+++ examples/example-create-s3-and-codepipeline/main.tf
@@ -1,14 +1,14 @@
 module "example-create-s3-and-codepipeline" {
   source = "../../modules/landing_zone"
   landing_zone_components = {
-      landing_zone_pipeline_s3_bucket = "s3://{BUCKET NAME}/{BUCKET KEY}/default.tfvars"
-      landing_zone_pipeline_artifact_s3_bucket = "s3://{BUCKET NAME}/{BUCKET KEY}/default.tfvars"
-      landing_zone_code_build_role = "s3://{BUCKET NAME}/{BUCKET KEY}/default.tfvars"
-      landing_zone_code_build_role_policy = "s3://{BUCKET NAME}/{BUCKET KEY}/default.tfvars"
-      landing_zone_code_build = "s3://{BUCKET NAME}/{BUCKET KEY}/default.tfvars"
-      landing_zone_code_pipeline_role = "s3://{BUCKET NAME}/{BUCKET KEY}/default.tfvars"
-      landing_zone_code_pipeline_role_policy = "s3://{BUCKET NAME}/{BUCKET KEY}/default.tfvars"
-      landing_zone_code_pipeline = "s3://{BUCKET NAME}/{BUCKET KEY}/default.tfvars"
+      landing_zone_pipeline_s3_bucket = "s3://terraform-aws-landing-zone/example/landing_zone_pipeline_s3_bucket/default.tfvars"
+      landing_zone_pipeline_artifact_s3_bucket = "s3://terraform-aws-landing-zone/example/landing_zone_pipeline_artifact_s3_bucket/default.tfvars"
+      landing_zone_code_build_role = "s3://terraform-aws-landing-zone/example/landing_zone_code_build_role/default.tfvars"
+      landing_zone_code_build_role_policy = "s3://terraform-aws-landing-zone/example/landing_zone_code_build_role_policy/default.tfvars"
+      landing_zone_code_build = "s3://terraform-aws-landing-zone/example/landing_zone_code_build/default.tfvars"
+      landing_zone_code_pipeline_role = "s3://terraform-aws-landing-zone/example/landing_zone_code_pipeline_role/default.tfvars"
+      landing_zone_code_pipeline_role_policy = "s3://terraform-aws-landing-zone/example/landing_zone_code_pipeline_role_policy/default.tfvars"
+      landing_zone_code_pipeline = "s3://terraform-aws-landing-zone/example/landing_zone_code_pipeline/default.tfvars"
   }
   region = "us-east-1"
   account_id = "123456789012"
