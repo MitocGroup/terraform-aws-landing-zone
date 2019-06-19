eb20b425ec18b78dba248b26398f7c3a219db036
root <root@DESKTOP-ECSQPB1.localdomain>
2019-06-19 17:54:43 +0200

    un-commit

--- modules/landing_zone/scripts/destroy.py
+++ modules/landing_zone/scripts/destroy.py
@@ -3,16 +3,16 @@ import json
 from libs import cli
 
 def main():
-    # components = eval(os.environ['components'])
+    components = eval(os.environ['components'])
 
-    # include = []
-    # for (k, v) in components.items():
-    #     include.append(k)
-    # include = ','.join(include)
-    # (error, result) = cli(['terrahub', 'destroy', '-i', include, '-y', '-p', 'ignore'], os.environ['root'])
-    # if error != 0:
-    #     print("Error: failed to execute command:")
-    #     raise Exception(result)
+    include = []
+    for (k, v) in components.items():
+        include.append(k)
+    include = ','.join(include)
+    (error, result) = cli(['terrahub', 'destroy', '-i', include, '-y', '-p', 'ignore'], os.environ['root'])
+    if error != 0:
+        print("Error: failed to execute command:")
+        raise Exception(result)
 
     return 'Success'
 
