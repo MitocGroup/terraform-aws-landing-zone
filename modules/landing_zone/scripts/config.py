30ff388526eb540e1b4d7a5225a120a0c56ea502
root <root@DESKTOP-ECSQPB1.localdomain>
2019-06-19 18:21:22 +0200

    fixed codeclimate issues

--- modules/landing_zone/scripts/config.py
+++ modules/landing_zone/scripts/config.py
@@ -6,39 +6,39 @@ def main():
     components = eval(os.environ['components'])
     aliases    = eval(os.environ['aliases'])
 
-    processes = []
+    config_processes = []
     index = 1
     for (k, v) in aliases.items():
         if k != 'default':
             index += 1
             default = 'template.provider[' + str(index) + ']'
-            processes.append(['terrahub', 'configure', '-c',
+            config_processes.append(['terrahub', 'configure', '-c',
                 default +'={}'])
-            processes.append(['terrahub', 'configure', '-c',
+            config_processes.append(['terrahub', 'configure', '-c',
                 default +'.aws={}'])
-            processes.append(['terrahub', 'configure', '-c',
+            config_processes.append(['terrahub', 'configure', '-c',
                 default + '.aws.alias=' + k])
-            processes.append(['terrahub', 'configure', '-c',
+            config_processes.append(['terrahub', 'configure', '-c',
                 default + '.aws.region=${var.'+ k + '_region}'])
-            processes.append(['terrahub', 'configure', '-c',
+            config_processes.append(['terrahub', 'configure', '-c',
                 default + '.aws.assume_role[0]={}'])
             config = '\'arn:aws:iam::${var.' + k + '_account_id}:role/OrganizationAccountAccessRole\''
-            processes.append(['terrahub', 'configure', '-c',
+            config_processes.append(['terrahub', 'configure', '-c',
                 default + '.aws.assume_role[0].role_arn=' + config])
-            processes.append(['terrahub', 'configure', '-c',
+            config_processes.append(['terrahub', 'configure', '-c',
                 default + '.aws.assume_role[0].session_name=${var.' + k + '_account_id}'])
         for (key_sub, val_sub) in v.items():
             if k == 'default':
-                processes.append(['terrahub', 'configure', '-c',
+                config_processes.append(['terrahub', 'configure', '-c',
                     'template.tfvars.' + key_sub + '=' + val_sub])
             else:
-                processes.append(['terrahub', 'configure', '-c',
+                config_processes.append(['terrahub', 'configure', '-c',
                     'template.tfvars.'+ k + '_' + key_sub + '=' + val_sub])
 
     for (k, v) in components.items():
         execWithoutErrors(['terrahub', 'configure', '-i', k, '-c', 'terraform', '-D', '-y'])
-        processes.append(['terrahub', 'configure', '-i', k, '-c', 'terraform.varFile[0]=' + str(v)])
-    execWithErrors(processes)
+        config_processes.append(['terrahub', 'configure', '-i', k, '-c', 'terraform.varFile[0]=' + str(v)])
+    execWithErrors(config_processes)
     return 'Success'
 
 if __name__ == '__main__':
