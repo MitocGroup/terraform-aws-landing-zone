f6bf40922f401eaa18c008ea8161dbb9b646876e
eistrati <eistrati@users.noreply.github.com>
2019-06-20 09:47:43 -0400

    add missing tfvars values

--- modules/landing_zone/scripts/remove_config.py
+++ modules/landing_zone/scripts/remove_config.py
@@ -17,6 +17,8 @@ def main():
     execWithoutErrors(thub_cfg + ['template.provider[1].aws.alias=default'], os.environ['root'])
     execWithoutErrors(thub_cfg + ['template.provider[1].aws.region=${var.region}'], os.environ['root'])
     execWithoutErrors(thub_cfg + ['template.provider[1].aws.allowed_account_ids[]=${var.account_id}'], os.environ['root'])
+    execWithoutErrors(thub_cfg + ['template.tfvars.account_id=123456789012'], os.environ['root'])
+    execWithoutErrors(thub_cfg + ['template.tfvars.region=us-east-1'], os.environ['root'])
     execWithoutErrors(thub_cfg + ['template.tfvars.tfstate_path='
         + '/tmp/.terrahub/tfstate_local/terraform-aws-landing-zone'], os.environ['root'])
     
