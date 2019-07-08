6c5dedcba51145c7f553ee9496faaf17abbd04bf
Eugene I <eugene@mitocgroup.com>
2019-07-08 11:32:40 -0400

    remove unnessary folders

--- demo/output.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  value       = "${aws_lambda_function.my_demo.id}"
-  description = "The ID of the Lambda function."
-}
-
-output "arn" {
-  value       = "${aws_lambda_function.my_demo.arn}"
-  description = "The ARN of the Lambda function."
-}
