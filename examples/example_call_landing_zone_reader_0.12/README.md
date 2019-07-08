b6e9bac03618f26e4d9e02633ac679a3a9a1edb7
root <root@DESKTOP-ECSQPB1.localdomain>
2019-07-08 15:26:44 +0200

    remove v.0.12

--- examples/example_call_landing_zone_reader_0.12/README.md
+++
@@ -1,2 +0,0 @@
-# example_call_landing_zone_reader_0.12
-Example for AWS Landing Zone
--- modules/landing_zone_reader_0.12/data.tf
+++
@@ -1,4 +0,0 @@
-data "local_file" "landing_zone_reader_output_file" {
-  depends_on = [null_resource.landing_zone_reader_apply]
-  filename   = "${var.root_path}/output.json"
-}
